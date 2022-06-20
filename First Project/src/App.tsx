27aa8a7e695f7edc7d4dd82f0dba7de7202523aa
Matheus Pesegoginski <matheuspese.dev@outlook.com>
2022-06-20 00:05:24 -0300

    modified placeholders of movie inputs

--- First Project/src/App.tsx
+++ First Project/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import MovieList from './MovieList';
+
 const App = () => {
   return (
   <>
-  <h1>Marvel's watched</h1>
+  <h1>Marvel's watchlist</h1>
   <h2>Every movie and series watched from marvel</h2>
 
   <MovieList/>
   <label>
-    <input></input>
+    <input placeholder='Title'></input>
+    <select name="Status">
+      <option value="Not Watched">Not Watched</option>
+      <option value="Watched">Watched</option>
+      <option value="Droped">Droped</option>
+      <option value="Plan to watch">Plan to watch</option>
+    </select>
     <button>add movie</button>
   </label>
  </>
