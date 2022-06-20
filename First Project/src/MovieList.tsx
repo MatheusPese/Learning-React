403f1e94d27d74dd99fd3bb6c10571e0dab7a2a4
Matheus Pesegoginski <matheuspese.dev@outlook.com>
2022-06-20 00:05:55 -0300

    implemented movielist with placeholder data

--- First Project/src/MovieList.tsx
+++ First Project/src/MovieList.tsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
 
+type MovieProps = {movie_index:number}
+let movies = [{
+    id:0,
+    title:"Spiderman Far From Home",
+    status:"Watched"
+},
+{
+    id:1,
+    title:"Doctor Strange (2016)",
+    status:"Plan to watch"
+},
+{
+    id:2,
+    title:"Doctor Strange in the Multiverse of Madness",
+    status:"Watched"
+}
+]
+const Movies = (props:MovieProps) => {
+
+    return <div className='movie'>
+    <div title={movies[props.movie_index].title}>{movies[props.movie_index].title}</div>
+    <div >Status: {movies[props.movie_index].status}</div>
+    </div>
+}
+
 const MovieList = () =>{
     return (
-        <div>MovieList</div>
-  
+        <>
+        <h3>MovieList:</h3>
+        <div className="movieList">
+        {movies.map((movie) => <Movies movie_index={movie.id} />)}
+        </div>
+        <br/>
+        </>
       )
 }
     
