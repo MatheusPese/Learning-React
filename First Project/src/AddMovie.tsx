b00194d303ec0416f78bccd57f3fa45a935a3fff
Matheus Pesegoginski <matheuspese.dev@outlook.com>
2022-06-20 23:29:51 -0300

    added input autocompletion (mui + themoviedb API)

--- First Project/src/AddMovie.tsx
+++ First Project/src/AddMovie.tsx
@@ -1,22 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
-const API_URL ='https://api.themoviedb.org/3/movie/550?api_key=030edf28c08ee51a7e306dc49727bc5e'
+***REMOVED***
+const API_URL ***REMOVED***
 
-export class AddMovie extends Component {
-  render() {
+const AddMovie = () => {
+    const [searchInput, setSearchInput] = useState<string>("");
+    const [searchQuery, setSearchQuery] = useState<any>([]);
+    
+    useEffect(()=>{
+      MovieSearch(searchInput)
+    },[searchInput])
+
+    const MovieSearch = async (query:string) =>{
+      if (query && searchInput.length >= 4){
+        const response = await fetch(`${API_URL}&query=${query}`)
+        const data = await response.json();
+        const results  = await data.results.map((x:any) => x.original_language =="en"? x.title:"")
+        console.log(results)
+        const uniqueResults = [...(new Set(results))]
+
+        setSearchQuery(uniqueResults)
+      }
+      else{
+        setSearchQuery([])
+      }
+    }
+    const changeAutocomplete = (e:any) => {
+      setSearchInput(e.target.value);
+      MovieSearch(searchInput); 
+    }
+
     return (
         <label>
-        <input placeholder='Title'></input>
+          <Autocomplete
+          id="teste"
+          options={searchQuery? searchQuery:""}
+          sx={{width:300}}
+          renderInput={(params) => <TextField {...params} onChange={(e) => changeAutocomplete(e)} onClick={(e) => changeAutocomplete(e)} label="Movie" />}
+          />
         <select name="Status">
           <option value="Not Watched">Not Watched</option>
           <option value="Watched">Watched</option>
           <option value="Droped">Droped</option>
           <option value="Plan to watch">Plan to watch</option>
         </select>
-        <button>add movie</button>
+        <button onClick={() => {}}>add movie</button>
       </label>
     )
-  }
 }
 
 export default AddMovie
